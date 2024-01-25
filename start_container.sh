df9fc732a65930da5c59d562ccaf710cd4568768
AlessandroAlbanesi <alessandro.albanesi@studio.unibo.it>
2024-01-26 00:38:20 +0100

    update

--- start_container.sh
+++ start_container.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-
+sudo rm -rf mount
 
 PGDATA_ROOT=/var/lib/postgresql/data
 
 docker run -d \
     --name deception-db \
-    -p 5433:5432 \
+    -p 5432:5432 \
     -e POSTGRES_PASSWORD=password \
     -e PGDATA=${PGDATA_ROOT}/pg_data \
     -v $(pwd)/mount:${PGDATA_ROOT} \
     -v "$(pwd)/image_configuration/postgres_configurations/postgres.conf":/home/postgres/postgres_conf/postgres.conf \
     -v "$(pwd)/image_configuration/postgres_configurations/pg_hba.conf":/home/postgres/postgres_conf/pg_hba.conf \
-    deception-db:latest \
+    alex176/deception_db:latest \
     -c "config_file=/home/postgres/postgres_conf/postgres.conf" \
     -c "hba_file=/home/postgres/postgres_conf/pg_hba.conf"
 
